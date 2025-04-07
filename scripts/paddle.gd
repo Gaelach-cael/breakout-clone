10255d4a2a01bc22fa7b7d0ba716651741cb0960
Gaelach-cael <cael.viala@gedu.sd73.bc.ca>
2025-04-07 13:21:17 -0700

    finally got it werking

--- scripts/paddle.gd
+++ scripts/paddle.gd
@@ -1,17 +1,18 @@
 extends CharacterBody2D
 
 #defines speed for paddle
-@export var paddle_speed = 150
+@export var paddle_speed = 25
+
 
 #function called per frame
-func _physics_process(_delta: float):
-	#ddefines movement var as 0 at start of frame
-	var movement = 0
-	#defines movement left+right
-	if Input.is_key_pressed(KEY_A):
-		movement -= 1
-	if Input.is_key_pressed(KEY_D):
-		movement += 1
+func _physics_process(delta: float):
+    #defines movement var as 0 at start of frame
+    var movement = 0
+    #defines movement left+right
+    if Input.is_action_pressed("move_left"):
+        movement -= 1
+    if Input.is_action_pressed("move_right"):
+        movement += 1
 
-	velocity = Vector2(movement, 0)*paddle_speed
-	move_and_slide()
+    velocity = Vector2(movement, 0) * paddle_speed
+    move_and_slide()
