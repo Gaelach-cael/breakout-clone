7210583802af03325fd6048e807a382a71953774
Gaelach-cael <cael.viala@gedu.sd73.bc.ca>
2025-04-09 14:03:38 -0700

    scoring

--- scripts/brick.gd
+++ scripts/brick.gd
@@ -1,6 +1,8 @@
 extends StaticBody2D
 
+@onready var score_node = get_node("score")
+
 func _on_area_2d_body_entered(body: Node2D):
         if body.is_in_group("Ball"):
+                score_node.score += 100
                 queue_free()
-        
---
+++ scripts/label.gd
@@ -0,0 +1,6 @@
+extends Label
+
+var score:int = 0
+
+func _process(_delta):
+    text = "Score:%d" %score
